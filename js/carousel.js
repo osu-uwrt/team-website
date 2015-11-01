4c2f09a91ee9dedc91079fc649c9e1836eacca39
Conor Cary <cary.42@osu.edu>
2015-11-01 17:06:00 -0500

    Testing slick.

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slide-renders').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    cssEase: 'linear',
+    dots: true,
+    fade: true,
+    infinite: true,
+    speed: 600
+  });
+});
