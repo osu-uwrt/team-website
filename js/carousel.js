2954b6e7754dce717d6a24e7e3efec53cab828f2
Conor Cary <cary.42@osu.edu>
2015-11-02 17:23:16 -0500

    Added more images and little tweaks.

--- js/carousel.js
+++ js/carousel.js
@@ -5,8 +5,28 @@ $(document).ready(function(){
     arrows: false,
     cssEase: 'linear',
     dots: true,
-    fade: true,
+    fade: false,
     infinite: true,
     speed: 600
+  });
+  $('.slide-top').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    cssEase: 'linear',
+    dots: false,
+    fade: true,
+    infinite: true,
+    speed: 400
+  });
+  $('.slide-bot').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    cssEase: 'linear',
+    dots: false,
+    fade: true,
+    infinite: true,
+    speed: 800
   });
 });
